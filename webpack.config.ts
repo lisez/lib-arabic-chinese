6d9640ec8a60f048481da7e4ac6677884386031d
lisez <mm4324@gmail.com>
2019-06-16 16:48:48 +0800

    [chore] Setup webpack bundle tools

---
+++ webpack.config.ts
@@ -0,0 +1,20 @@
+import { Configuration } from 'webpack';
+
+type TNodeEnv = 'development' | 'production';
+
+const nodeEnv: TNodeEnv = <TNodeEnv>process.env.NODE_ENV;
+
+const webpackConfig: Configuration = {
+  mode: nodeEnv || 'development',
+  devtool: nodeEnv === 'development' ? 'source-map' : false,
+  entry: './src/index.ts',
+  output: {
+    filename: './bundle.js'
+  },
+  resolve: { extensions: ['.ts', '.tsx', '.js'] },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }]
+  }
+};
+
+export default webpackConfig;
