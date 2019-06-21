fc3bb01f9701fc1c1fab68aabc7d1a9f57b671cd
lisez <mm4324@gmail.com>
2019-06-21 21:10:21 +0800

    [feat] Support BigInt

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,10 +4,30 @@ import TestCases from './testcases.json';
 for (const testset of TestCases) {
   describe(testset.set, () => {
     for (let [tNumber, eValue] of Object.entries(testset.data)) {
-      test(`${tNumber} -> ${eValue}`, () => {
+      test(`(String) ${tNumber} -> ${eValue}`, () => {
         const aValue = Converter(tNumber, <IConverterConfig>(testset.options || {}));
         expect(aValue).toEqual(eValue);
       });
+
+      // to Number
+      if (!tNumber.startsWith('+')) {
+        const noComma = tNumber.replace(/,/g, '');
+        const toNumber = Number(noComma);
+        if (toNumber <= Number.MAX_SAFE_INTEGER && !Number.isNaN(toNumber)) {
+          test(`(Number) ${toNumber.toString()} -> ${eValue}`, () => {
+            const opts = <IConverterConfig>(testset.options || {});
+            const nValue = Converter(toNumber, opts);
+            expect(nValue).toEqual(eValue);
+          });
+        } else if (BigInt) {
+          const toBigInt = BigInt(noComma);
+          test(`(BigInt) ${toBigInt.toString()} -> ${eValue}`, () => {
+            const opts = <IConverterConfig>(testset.options || {});
+            const nValue = Converter(toBigInt, opts);
+            expect(nValue).toEqual(eValue);
+          });
+        }
+      }
     }
   });
 }
